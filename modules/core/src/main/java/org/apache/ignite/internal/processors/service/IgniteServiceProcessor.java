71e00d0958dde73db232e314879b31e9a88fda13
Arthur Naseef <art@artnaseef.com>
2022-08-03 15:57:57 -0700

    Improve lookup of deployed services UUID by name: replace list lookup with map lookup.

--- modules/core/src/main/java/org/apache/ignite/internal/processors/service/IgniteServiceProcessor.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/service/IgniteServiceProcessor.java
@@ -190,6 +190,12 @@ public class IgniteServiceProcessor extends GridProcessorAdapter implements Igni
      */
     private final ConcurrentMap<IgniteUuid, ServiceInfo> deployedServices = new ConcurrentHashMap<>();
 
+    /**
+     * Map of deployed services, kept in-sync with deployedServices above, and keyed by name to improve performance
+     * for lookups.
+     */
+    private final ConcurrentMap<String, ServiceInfo> deployedServicesByName = new ConcurrentHashMap<>();
+
     /** Deployment futures. */
     private final ConcurrentMap<IgniteUuid, GridServiceDeploymentFuture<IgniteUuid>> depFuts = new ConcurrentHashMap<>();
 
@@ -347,6 +353,7 @@ public class IgniteServiceProcessor extends GridProcessorAdapter implements Igni
         assert opsLock.isWriteLockedByCurrentThread();
 
         deployedServices.clear();
+        deployedServicesByName.clear();
 
         ctx.metric().remove(SERVICE_METRIC_REGISTRY);
 
@@ -1555,9 +1562,9 @@ public class IgniteServiceProcessor extends GridProcessorAdapter implements Igni
      * @return @return Service's id if exists, otherwise {@code null};
      */
     @Nullable private IgniteUuid lookupDeployedServiceId(String name) {
-        for (ServiceInfo desc : deployedServices.values()) {
-            if (desc.name().equals(name))
-                return desc.serviceId();
+        ServiceInfo serviceInfo = deployedServicesByName.get(name);
+        if (serviceInfo != null) {
+            return serviceInfo.serviceId();
         }
 
         return null;
@@ -1590,12 +1597,18 @@ public class IgniteServiceProcessor extends GridProcessorAdapter implements Igni
             return;
 
         try {
-            depActions.servicesToDeploy().forEach(deployedServices::putIfAbsent);
+            depActions.servicesToDeploy().forEach((uuid, serviceInfo) -> {
+                if (deployedServices.putIfAbsent(uuid, serviceInfo) == null) {
+                    deployedServicesByName.put(serviceInfo.name(), serviceInfo);
+                }
+            });
 
             depActions.servicesToUndeploy().forEach((srvcId, desc) -> {
                 ServiceInfo rmv = deployedServices.remove(srvcId);
 
                 assert rmv != null && rmv == desc : "Concurrent map modification.";
+
+                deployedServicesByName.remove(rmv.name());
             });
         }
         finally {
